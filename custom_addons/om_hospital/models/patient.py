a99b33d0b9721b9b8c45ef1791c05bad591b96ef
fageer <fageern@gmail.com>
2024-04-17 12:58:51 +0300

    Day4

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -5,8 +5,8 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name')
-    ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
+    name = fields.Char(string='Name', tracking=True)
+    ref = fields.Char(string='Reference', tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
     active = fields.Boolean(string='Active', default=True)
