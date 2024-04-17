c49812e1a96f96323931e93614ff389400d6e605
fageer <fageern@gmail.com>
2024-04-17 09:07:46 +0300

    Odoo15

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Fager Mohsen',
+    'sequence': -100,
+    'summary': 'Hospital Management Sysytem',
+    'description': """ Hospital Management Sysytem """,
+    'depends': [],
+    'data': [
+        'security\ir.model.access.csv',
+        'views\menu.xml',
+        'views\patient_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+    'assets': {},
+}
