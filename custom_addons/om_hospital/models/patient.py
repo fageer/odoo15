51c6e11db11ee2754a48af14a9a818f9af52c87e
fageer <fageern@gmail.com>
2024-04-17 11:56:47 +0300

    Day 4

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -2,9 +2,11 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
 
     name = fields.Char(string='Name')
     ref = fields.Char(string='Reference')
     age = fields.Integer(string='Age')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
+    active = fields.Boolean(string='Active', default=True)
