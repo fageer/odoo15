7de7c5192493ed7a03881fbeb4fda709331702e8
fageer <fageern@gmail.com>
2024-04-18 08:28:38 +0300

    Day5

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -7,8 +7,13 @@ class HospitalAppointment(models.Model):
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     gender = fields.Selection( related='patient_id.gender')
-
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference', tracking=True, default='HP00')
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
+
 
 
