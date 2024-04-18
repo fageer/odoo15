26f6b22fec0c9f9f007842143686bb787f418b72
fageer <fageern@gmail.com>
2024-04-18 15:31:38 +0300

    Badge Widget and Decorations, Day5

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -1,10 +1,12 @@
 from odoo import api, fields, models
 
+
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
     _rec_name = 'ref'
+
 
     patient_id = fields.Many2one('hospital.patient', string='Patient', help='Patient name')
     gender = fields.Selection(related='patient_id.gender')
@@ -12,18 +14,18 @@ class HospitalAppointment(models.Model):
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today, help='Date of booking')
     ref = fields.Char(string='Reference', tracking=True, default='HP00', help='This is reference to the patient record')
     prescription = fields.Html(string='Prescription')
-
     priority = fields.Selection([
         ('0', 'Normal'),
         ('1', 'Low'),
         ('2', 'High'),
         ('3', 'Very High')], string='Priority')
-
     state = fields.Selection([
         ('draft', 'Draft'),
         ('in_consultation', 'In Consultation'),
         ('done', 'Done'),
         ('cancel', 'Cancelled')], default='draft', string='Status', required=True)
+
+
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
