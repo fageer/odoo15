b4f96dbea5fce080bb57f18951d5f00e32e4317b
fageer <fageern@gmail.com>
2024-04-24 15:02:18 +0300

    Inherit A Function, Day9

--- custom_addons/om_odoo_inheritence/models/sale_order.py
+++ custom_addons/om_odoo_inheritence/models/sale_order.py
@@ -4,3 +4,9 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
+
+
+    def action_confirm(self):
+        print("Success !!!!!!!!!!!!!!!!!!")
+        super(SaleOrder, self).action_confirm()
+        self.confirmed_user_id = self.env.user.id
