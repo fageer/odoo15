494fa68b0ef42cad7c50ec53bd67c21e9626d272
fageer <fageern@gmail.com>
2024-04-25 09:27:57 +0300

    Override Default Get Function, Day10

--- custom_addons/om_hospital/wizard/cancel_appointment.py
+++ custom_addons/om_hospital/wizard/cancel_appointment.py
@@ -1,3 +1,4 @@
+import datetime
 from odoo import api, fields, models
 
 
@@ -5,8 +6,17 @@ class CancelAppointmentWizard(models.TransientModel):
     _name = "cancel.appointment.wizard"
     _description = 'Cancel Appointment Wizard'
 
+
+    @api.model
+    def default_get(self, fields):
+        res = super(CancelAppointmentWizard, self).default_get(fields)
+        res['date_cancel'] = datetime.date.today()
+        return res
+
+
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
     reason = fields.Text(string='Reason')
+    date_cancel = fields.Date(string='Cancellation Date')
 
     def action_cancel(self):
         return
