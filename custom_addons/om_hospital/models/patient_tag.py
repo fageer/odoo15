6143f41a15668896451fd22a40025527d248bc88
fageer <fageern@gmail.com>
2024-04-28 16:18:41 +0300

    Sql Constraints, Day10

--- custom_addons/om_hospital/models/patient_tag.py
+++ custom_addons/om_hospital/models/patient_tag.py
@@ -10,3 +10,11 @@ class PatientTag(models.Model):
     active = fields.Boolean(string='Active', default=True)
     color = fields.Integer(string='Color')
     color_2 = fields.Char(string='Color 2')
+    sequence = fields.Integer(string='Sequence')
+
+
+    _sql_constraints = [
+        ('unique_tag_name', 'unique (name)', 'Name Already Exists.'),
+        ('check_sequence', 'check (sequence > 0)', 'sequence must be greater than zero.')
+    ]
+    
