e9ab5b1f6ecd4ae2ce4469080508c73ab2592d67
fageer <fageern@gmail.com>
2024-04-30 17:25:30 +0300

    Copy Function, Day13

--- custom_addons/om_hospital/models/patient_tag.py
+++ custom_addons/om_hospital/models/patient_tag.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class PatientTag(models.Model):
@@ -7,11 +7,20 @@ class PatientTag(models.Model):
 
 
     name = fields.Char(string='Name', required=True)
-    active = fields.Boolean(string='Active', default=True)
+    active = fields.Boolean(string='Active', default=True, copy=False)
     color = fields.Integer(string='Color')
     color_2 = fields.Char(string='Color 2')
     sequence = fields.Integer(string='Sequence')
 
+    
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        if not default.get('name'):
+            default['name'] = _(f"{self.name} (Copy)")
+        default['sequence'] = 10
+        return super(PatientTag, self).copy(default)
 
     _sql_constraints = [
         ('unique_tag_name', 'unique (name)', 'Name Already Exists.'),
