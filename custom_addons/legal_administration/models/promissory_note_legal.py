d09f9e1f1ef109399c345272a10cb47c8c2fbffe
fageer <fageern@gmail.com>
2024-05-01 16:17:08 +0300

    Legal Administration Module Task, Day14

--- custom_addons/legal_administration/models/promissory_note_legal.py
+++ custom_addons/legal_administration/models/promissory_note_legal.py
@@ -7,12 +7,17 @@ class PromissoryNoteLegal(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = 'bond_issuer'
     
-    bond_issuer = fields.Char(string='Bond Issuer')
+    bond_issuer = fields.Char(string='Bond Issuer', compute='_compute_bond_issuer', store=True)
     document_status =  fields.Selection([
         ('new', 'New'),
         ('in_progress', 'In Progress'),
         ('done', 'Done')], string='Document Status', required=True)
     
+    @api.depends('document_status')
+    def _compute_bond_issuer(self):
+        for rec in self:
+            rec.bond_issuer = self.env.user.company_id.name
+        
     
     
     
