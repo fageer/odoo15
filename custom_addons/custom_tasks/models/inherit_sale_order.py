17d1e035767f50d3fa610570689d35d7ec4ace79
fageer <fageern@gmail.com>
2024-05-28 12:59:57 +0300

    Inheritance Tasks

--- custom_addons/custom_tasks/models/inherit_sale_order.py
+++ custom_addons/custom_tasks/models/inherit_sale_order.py
@@ -13,49 +13,40 @@ class SaleOrder(models.Model):
         if self.bid_type == 'cash':
             if not self.payment_info_ids:
                 raise ValidationError(_('Please provide payment information.'))
-            
             total_payment = sum(payment_info.total for payment_info in self.payment_info_ids)
-            
             for payment_info in self.payment_info_ids:
                 if not payment_info.payment_type_id or payment_info.total <= 0:
                     raise ValidationError(_('Invalid payment type or total amount in payment information.'))
             if not float_is_zero(self.amount_total - total_payment, precision_rounding=self.currency_id.rounding):
                 raise ValidationError(_('Please check the total amount.'))    
             super(SaleOrder, self).action_confirm()
-            
             self._create_invoices()
-            print("eeeeeeeeeeeeeeeeeeeeeeee")
             payments = []
             for pay in self.payment_info_ids:
                 payment_vals = {
                     'amount': pay.total,
-                    # 'payment_date': fields.Datetime.now(),
+                    'date': fields.Datetime.now(),
                     'payment_method_id':1,
-                    # 'ref': self.invoice_ids.name,
                     'payment_type': 'inbound',
                     'partner_type': 'customer',
                     'partner_id': self.partner_id.id,
                     'journal_id': pay.payment_type_id.id,
                     'currency_id': self.currency_id.id,
-                }
+                    }
                 payments.append(payment_vals)
-                print("*** * * * * **      ", payment_vals)
             for payment in payments:
                 self.env['account.payment'].create(payment).action_post()
         
         else:
             invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice'),
+                                                        ('state', '=', 'posted'),
+                                                        ('payment_state', '=', 'not_paid'),
                                                         ('partner_id', '=', self.partner_id.id)])
             total_un_paid = 0
-            for invoice in invoices:
-                if invoice.state == 'posted':
-                    if invoice.payment_state == 'not_paid':
-                        total_un_paid += invoice.amount_residual
+            total_un_paid = sum(map(lambda invoice: invoice.amount_residual, invoices))
             total_un_paid += self.amount_total
             remaining_credit = self.partner_id.limit - float(total_un_paid)
             if int(remaining_credit) <= 0:
-                print("=======================================================================================================", remaining_credit)
-                print("=======================================================================================================", total_un_paid)
                 raise ValidationError(_('Please you have reach limited amount.'))
             else:
                 super(SaleOrder, self).action_confirm()
