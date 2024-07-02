37f38eddd7266c5eb6fadf11c0d69357f906ad15
fageer <fageern@gmail.com>
2024-07-02 17:22:04 +0300

    Branches Task

--- custom_addons/branch_task/controllers/branch_api.py
+++ custom_addons/branch_task/controllers/branch_api.py
@@ -2,10 +2,16 @@ import json
 from odoo import http
 from odoo.http import request
 
+
 class BranchApi(http.Controller):
     @http.route("/v1/branch", methods=["POST"], type="json", auth="public", csrf=False)
     def post_branch(self, **kw):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
-        res = request.env['res.partner'].create(vals)
-        print(res)
+        for key, val in vals.items():
+            res = request.env['branch.branch'].sudo().create(val)
+            print(res)
+        print("=================================================")
+        print("inside post_branch method")
+        print("inside post_branch method")
+        print("=================================================")
--- custom_addons/branch_task/models/branch.py
+++ custom_addons/branch_task/models/branch.py
@@ -1,20 +1,17 @@
 from odoo import api, fields, models, _
 
+
 class BranchBranch(models.Model):
     _name = "branch.branch"
     _description = "Branches"
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    
-    
+
     ref = fields.Char(string='Referance', readonly=True)
     name = fields.Char(string='Name', required=True)
     employees_ids = fields.Many2many('res.users', string='Employees')
     responsible_id = fields.Many2one('res.users', string='Responsible')
     location_id = fields.Many2one('stock.location', string='Location', required=True)
-    
-    
-    
-    
+
     @api.model
     def create(self, vals):
         vals['ref'] = self.env['ir.sequence'].next_by_code('branch.branch')
