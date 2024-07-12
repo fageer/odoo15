7fa702b5acfcafd1cd984caa8d945d2203a0ee5e
fageer <fageern@gmail.com>
2024-07-12 20:21:51 +0300

    some changes

--- custom_addons/om_odoo_inheritence/models/sale_order.py
+++ custom_addons/om_odoo_inheritence/models/sale_order.py
@@ -4,6 +4,8 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
+
     def _action_confirm(self):
         super(SaleOrder, self)._action_confirm()
         for order in self:
@@ -11,3 +13,12 @@ class SaleOrder(models.Model):
                 moves = self.env['stock.move'].search([('sale_line_id', '=', line.id)])
                 for move in moves:
                     move.x_field = line.x_field
+
+    def action_confirm(self):
+        print("Success !!!!!!!!!!!!!!!!!!")
+        super(SaleOrder, self).action_confirm()
+        self.confirmed_user_id = self.env.user.id
+
+    def action_cancel(self):
+        print("Cancelled !!!!!!!!!!!!!!!!!!")
+        super(SaleOrder, self).action_cancel()
--- custom_addons/om_odoo_inheritence/models/sale_order_line.py
+++ custom_addons/om_odoo_inheritence/models/sale_order_line.py
@@ -4,14 +4,4 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
     x_field = fields.Char('X Field')
-
-    def action_confirm(self):
-        print("Success !!!!!!!!!!!!!!!!!!")
-        super(SaleOrderLine, self).action_confirm()
-        self.confirmed_user_id = self.env.user.id
-
-    def action_cancel(self):
-        print("Cancelled !!!!!!!!!!!!!!!!!!")
-        super(SaleOrderLine, self).action_cancel()
