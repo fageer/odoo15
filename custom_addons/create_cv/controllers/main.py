ef384572b55672e4d880600061a1e8841359d29e
fageer <fageern@gmail.com>
2024-07-21 17:16:25 +0300

    Web Controller

--- custom_addons/create_cv/controllers/main.py
+++ custom_addons/create_cv/controllers/main.py
@@ -27,6 +27,15 @@ class CreateCv(http.Controller):
 
     @http.route('/create/webcv', type="http", auth="public", website=True, csrf=True)
     def create_webcv(self, **kwargs):
+        # Retrieve selected skill IDs from the form
+        skills_ids = kwargs.get('skills_ids[]')
+        if skills_ids:
+            # Convert to list of integers
+            skills_ids = [int(skill_id) for skill_id in skills_ids]
+            # Format for Many2many field (6, 0, [ids])
+            skills_ids_command = [(6, 0, skills_ids)]
+        else:
+            skills_ids_command = [(6, 0, [])]  # Empty selection
         cv_vals = {
             'image': kwargs.get('image'),
             'name_id': kwargs.get('name_id'),
@@ -36,7 +45,7 @@ class CreateCv(http.Controller):
             'country_id': kwargs.get('country_id'),
             'city_id': kwargs.get('city_id'),
             'summary': kwargs.get('summary'),
-            'skills_ids': [[283, 0, [283]]],
+            'skills_ids': skills_ids_command,
         }
         request.env['create.cv'].sudo().create(cv_vals)
         return request.render('create_cv.cv_thanks', {})
