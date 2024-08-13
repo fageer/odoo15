e88f1fd8a6a184ac40226aed8b28d18134d34999
fageer <fageern@gmail.com>
2024-08-13 22:17:56 +0300

    CRM Task

--- custom_addons/crm_task/__manifest__.py
+++ custom_addons/crm_task/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Fager Mohsen",
     'website': "http://www.github.com/fageer",
     'version': '0.1',
-    'depends': ['base', 'crm', 'sale', 'purchase'],
+    'depends': ['base', 'crm', 'sale', 'purchase', 'sale_crm'],
     'data': [
         'security/ir.model.access.csv',
         'views/advertiser_view.xml',
