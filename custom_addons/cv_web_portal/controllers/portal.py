da8583665f21ed3695b2dd8ed0da039555f26086
fageer <fageern@gmail.com>
2024-08-13 14:05:26 +0300

    CVs Web portal

--- custom_addons/cv_web_portal/controllers/portal.py
+++ custom_addons/cv_web_portal/controllers/portal.py
@@ -26,24 +26,24 @@ class CvPortal(CustomerPortal):
             'pager': page_details,
         }
         return request.render('cv_web_portal.cvs_list_view_portal', vals)
-    #
-    # @http.route(['/my/booking/<model("booking.room"):booking_id>'], type='http', auth="user", website=True)
-    # def bookings_form_view(self, booking_id, **kwargs):
-    #     vals = {
-    #         "booking": booking_id,
-    #         'page_name': 'bookings_form_view',
-    #     }
-    #
-    #     booking_records = request.env['booking.room'].sudo().search([])
-    #     booking_ids = booking_records.ids
-    #     booking_index = booking_ids.index(booking_id.id)
-    #
-    #     if booking_index != 0 and booking_ids[booking_index - 1]:
-    #         vals['prev_record'] = '/my/booking/{}'.format(booking_ids[booking_index - 1])
-    #     if booking_index < len(booking_ids) - 1 and booking_ids[booking_index + 1]:
-    #         vals['next_record'] = '/my/booking/{}'.format(booking_ids[booking_index + 1])
-    #     return request.render("bookings_web_portal.bookings_form_view_portal", vals)
-    #
+
+    @http.route(['/my/cv/<model("create.cv"):cv_id>'], type='http', auth="user", website=True)
+    def cvs_form_view(self, cv_id, **kwargs):
+        vals = {
+            "cv": cv_id,
+            'page_name': 'cvs_form_view',
+        }
+
+        cv_records = request.env['create.cv'].sudo().search([])
+        cv_ids = cv_records.ids
+        cv_index = cv_ids.index(cv_id.id)
+
+        if cv_index != 0 and cv_ids[cv_index - 1]:
+            vals['prev_record'] = '/my/cv/{}'.format(cv_ids[cv_index - 1])
+        if cv_index < len(cv_ids) - 1 and cv_ids[cv_index + 1]:
+            vals['next_record'] = '/my/cv/{}'.format(cv_ids[cv_index + 1])
+        return request.render("cv_web_portal.cvs_form_view_portal", vals)
+
     # @http.route(['/my/booking/print/<model("booking.room"):booking_id>'], type='http', auth="user", website=True)
     # def bookings_report_print(self, booking_id, **kwargs):
     #     print("=============== calling", booking_id)
