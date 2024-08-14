0b8490d1b5a59d19104bd6325fab59577f0da095
fageer <fageern@gmail.com>
2024-08-14 06:54:27 +0300

    api

--- custom_addons/branch_task/controllers/branch_api.py
+++ custom_addons/branch_task/controllers/branch_api.py
@@ -12,13 +12,41 @@ class BranchApi(http.Controller):
             request.env['branch.branch'].sudo().create(val)
         return {'message': 'Branches Created Successfully'}
 
-    @http.route("/v1/branch/one", methods=["POST"], type="json", auth="public", csrf=False)
-    def post_branch_one(self, **kw):
+    # Create Branch Using ORM Method
+    @http.route("/v1/branch/one/orm", methods=["POST"], type="json", auth="public", csrf=False)
+    def post_branch_one_orm(self, **kw):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
         try:
-            request.env['branch.branch'].sudo().create(vals)
-            return {'message': 'Branch Created Successfully'}
+            res = request.env['branch.branch'].sudo().create(vals)
+            if res:
+                return {'message': 'Branch Created Successfully',
+                        'id': res.id,
+                        'name': res.name}
+        except Exception as error:
+            return {'error': error}
+
+    # Create Branch Using SQL Query
+    @http.route("/v1/branch/one/query", methods=["POST"], type="json", auth="public", csrf=False)
+    def post_branch_one_sql(self, **kw):
+        args = request.httprequest.data.decode()
+        vals = json.loads(args)
+        try:
+            cr = request.env.cr
+            columns = ", ".join(vals.keys())
+            values = ", ".join(['%s'] * len(vals))
+            query = f"""
+                INSERT INTO branch_branch ({columns})
+                VALUES ({values})
+                RETURNING id, name;
+            """
+            cr.execute(query, tuple(vals.values()))
+            res = cr.fetchone()
+            print(res)
+            if res:
+                return {'message': 'Branch Created Successfully',
+                        'id': res[0],
+                        'name': res[1]}
         except Exception as error:
             return {'error': error}
 
