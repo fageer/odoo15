5f6d7aa2ca3273807c097e9272d42d05d6075df6
fageer <fageern@gmail.com>
2024-08-19 23:20:27 +0300

    Some Changes

--- custom_addons/partner_message/models/sale_order.py
+++ custom_addons/partner_message/models/sale_order.py
@@ -4,25 +4,33 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.onchange('payment_term_id')
+    def onchange_payment_term_id(self):
+        partner_id = self.partner_id.id
+        partner = self.env['res.partner'].browse(partner_id)
+        message = f"""
+           Payment Term
+           <i class="fa fa-long-arrow-right" aria-label="Arrow icon" title="Arrow"/>
+           {self.payment_term_id.name}
+        """
+        partner.message_post(body=message)
+
     @api.model
     def create(self, vals):
         sale_order = super(SaleOrder, self).create(vals)
-        partner_id = sale_order.partner_id.id
-        partner = self.env['res.partner'].browse(partner_id)
+        partner = sale_order.partner_id
         sale_order_url = f"http://localhost:8066/web#cids=1&menu_id=243&action=398&model=sale.order&view_type=form&id={sale_order.id}"
         partner.message_post(body=f"Quotation [<a href='{sale_order_url}'>{sale_order.name}</a>] Created")
         return sale_order
 
     def action_cancel(self):
         super(SaleOrder, self).action_cancel()
-        partner_id = self.partner_id.id
-        partner = self.env['res.partner'].browse(partner_id)
+        partner = self.partner_id
         sale_order_url = f"http://localhost:8066/web#cids=1&menu_id=243&action=398&model=sale.order&view_type=form&id={self.id}"
         partner.message_post(body=f"Quotation [<a href='{sale_order_url}'>{self.name}</a>] Canceled")
 
     def action_confirm(self):
         super(SaleOrder, self).action_confirm()
-        partner_id = self.partner_id.id
-        partner = self.env['res.partner'].browse(partner_id)
+        partner = self.partner_id
         sale_order_url = f"http://localhost:8066/web#cids=1&menu_id=243&action=398&model=sale.order&view_type=form&id={self.id}"
         partner.message_post(body=f"Quotation [<a href='{sale_order_url}'>{self.name}</a>] Confirmed")
