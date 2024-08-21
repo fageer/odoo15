2ba3377eb9ba88fb023cbe08603c6e2c6e40d03b
fageer <fageern@gmail.com>
2024-08-21 22:32:19 +0300

    Res Partner Portal Task

--- custom_addons/res_partner_portal_task/__manifest__.py
+++ custom_addons/res_partner_portal_task/__manifest__.py
@@ -6,9 +6,10 @@
     'author': "Fager Mohsen",
     'website': "http://www.github.com/fageer",
     'version': '0.1',
-    'depends': ['base', 'contacts'],
+    'depends': ['base', 'contacts', 'portal', 'web'],
     'data': [
-        'views/res_partner_view.xml'
+        'views/res_partner_view.xml',
+        'views/res_partner_portal_form.xml',
     ],
     'demo': [],
     'installable': True,
