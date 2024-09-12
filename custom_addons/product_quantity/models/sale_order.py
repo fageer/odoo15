63f721058cb26c052fe9bc69b608f3354c73389a
fageer <fageern@gmail.com>
2024-09-12 17:03:23 +0300

    Stock Quantity

--- custom_addons/product_quantity/models/sale_order.py
+++ custom_addons/product_quantity/models/sale_order.py
@@ -1,15 +1,17 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # def _action_confirm(self):
-    #     super(SaleOrder, self)._action_confirm()
-    #     for order in self:
-    #         for line in order.order_line:
-    #             for move_id in line.move_ids:
-    #                 move_id.product_img = line.product_img
+    def _action_confirm(self):
+        super(SaleOrder, self)._action_confirm()
+        for order in self:
+            for line in order.order_line:
+                if line.product_uom_qty > line.product_id.qty_available:
+                    raise ValidationError(_(f'You Don\'t Have Enough Quantity From {line.product_id.name}.'))
+
 
 
 
